e7d91d243ed4456985f513f2c6841572d9d92ecc
Zender85 <damage85@mail.ru>
2017-02-15 21:59:25 +0300

    правки сетки и JS
    форма работает при отключенном джс, колонки в хедере по 20%

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,7 @@
       var form = document.querySelector('.hotel-searchform');
       var arrival = document.querySelector('#arrival-date');
       
+      form.classList.add('hidden');
       link.addEventListener('click', function (event) {
         event.preventDefault();
         form.classList.toggle('hotel-searchform-open');
